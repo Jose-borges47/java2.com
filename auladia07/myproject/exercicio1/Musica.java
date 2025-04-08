399b3c2ef26cb9128da13e1dc3593b1173125b8b
Jose-borges47 <jose_borges@discente.ufg.br>
2025-04-07 21:41:16 -0300

    exercicio dia 07/02

---
+++ auladia07/myproject/exercicio1/Musica.java
@@ -0,0 +1,13 @@
+package auladia07.myproject.exercicio1;
+
+public class Musica {
+    String nome;
+    int ano;
+    String tipo;
+
+    public Musica(String nome, String tipo, int ano) {
+        this.tipo = tipo;
+        this.ano = ano;
+        this.nome = nome;
+    }
+}
